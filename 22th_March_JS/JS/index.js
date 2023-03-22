a1125e9b8395ec94a1eb7fd27437bc5bb45390fb
Rupesh Tripathi <rupeshtripa@gmail.com>
2023-03-22 22:40:46 +0100

    js

---
+++ 22th_March_JS/JS/index.js
@@ -0,0 +1,12 @@
+console.log("hello");
+// js variables storing data values
+var name="Rupesh";
+console.log(name);
+console.log(typeof name);
+
+var num= 23;
+console.log(typeof num);
+
+var abc=true;
+console.log(typeof abc);
+document.write('Hello World, How are you?')
