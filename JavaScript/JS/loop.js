8c17804244ae9a92c7d8adfd908135f5366cef0a
Rupesh Tripathi <rupeshtripa@gmail.com>
2023-03-23 23:30:52 +0100

    loop

---
+++ JavaScript/JS/loop.js
@@ -0,0 +1,9 @@
+//for , while, dowhile, forin, for of, continue, break
+for (var a=0; a<10; a++){
+    if(a==3){
+        console.log("its 3")
+        //continue; break;
+        break;
+    }
+    console.log(a)
+}
