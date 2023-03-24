d325c981ac53230be4462bdb07143bb561c7dd69
Rupesh Tripathi <rupeshtripa@gmail.com>
2023-03-24 17:16:27 +0100

    loops js

--- JavaScript/JS/loop.js
+++ JavaScript/JS/loop.js
@@ -1,9 +1,25 @@
 //for , while, dowhile, forin, for of, continue, break
-for (var a=0; a<10; a++){
-    if(a==3){
-        console.log("its 3")
-        //continue; break;
-        break;
-    }
-    console.log(a)
+// for (var a=0; a<10; a++){
+//     if(a==3){
+//         console.log("its 3")
+//         //continue; break;
+//         break;
+//     }
+//     console.log(a)
+// }
+
+
+var c=0;
+do{
+    console.log("Do while loop", c)
+    c++
+}
+while(c<10)
+
+var aq=['ram', 'sita', 'gita']
+for(const aa of aq){
+    console.log(aa)
+}
+for(const aa in aq){
+    console.log(aa)
 }
