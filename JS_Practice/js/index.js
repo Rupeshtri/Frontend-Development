29c6686fe7f7f12eea9b57210ccd72655837d37e
Rupesh Tripathi <rupeshtripa@gmail.com>
2023-03-24 17:17:42 +0100

    js practice

---
+++ JS_Practice/js/index.js
@@ -0,0 +1,4 @@
+document.write("hello world </br>" );
+document.write("<b><i> I am Rupesh </i></b> ");
+document.write("<div> This is div  </div>");
+document.write("<section> This is another div section </section>");
